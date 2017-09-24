b8f99ec74ae35fdd9246bf68a7c818b3962b8f0b
Jason <jasonmjohnson1@gmail.com>
2017-09-24 01:25:28 -0400

    Create debugoutput.h

---
+++ src/debugoutput.h
@@ -0,0 +1,11 @@
+#ifndef DEBUGOUTPUT
+#define DEBUGOUTPUT
+#include <windows.h>
+#include <winsock.h>
+#include <stdio.h>
+#include "Dispatcher/exceptiondispatcher.h"
+
+void SendDbgRegisters(SOCKET Server, BOOLEAN ProtocolGUI, DWORD EIP, VirtualRegisters Registers);
+void SendDbgGet(SOCKET Server, BOOLEAN ExceptionType, PoolSect segment);
+
+#endif
