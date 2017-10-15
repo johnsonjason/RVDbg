e053e6849b9cf816e59f3655d4c8dd032960534c
Jason <jasonmjohnson1@gmail.com>
2017-10-15 15:47:54 -0400

    Update dllmain.cpp

--- src/dllmain.cpp
+++ src/dllmain.cpp
@@ -1,25 +1,31 @@
+// dllmain.cpp : Defines the entry point for the DLL application.
 // Programmed by jasonfish4
 /* The excessive use of global variables is due to me not wanting to consume stack space, overuse registers, etc...
 within an exception signaling function */
+
+#define _CRT_SECURE_NO_DEPRECATE
 
 #include "stdafx.h"
 #include "Dispatcher\rvdbg.h"
 #include "debugoutput.h"
 #include <winsock.h>
-#include <iostream>
 #include <string>
+#include <map>
 
 #pragma comment(lib, "wsock32.lib")
 
 BOOLEAN ProtocolGUI;
 
-const char* ModuleName; // Name of the image
-const char* SecModuleName; // Name of the copy image
-const char* Filename; // File location to copy image
+const char* ModuleName;
+const char* SecModuleName;
+const char* Filename;
 
 SOCKET Server;
 SOCKADDR_IN ServerAddress;
 HANDLE lThreads[2];
+std::map<char, DWORD> XMMRegister;
+std::map<char, DWORD> GeneralRegister;
+
 
 
 DWORD WINAPI Repeater(LPVOID lpParam)
@@ -27,13 +33,47 @@ DWORD WINAPI Repeater(LPVOID lpParam)
 	while (TRUE)
 	{
 		if (tDSend == FALSE)
-			Sleep(1);
-		else
 		{
-			tDSend = FALSE;
-			send(Server, "!DbgModeOn", sizeof("!DbgModeOn"), 0);
+			EnterCriticalSection(&repr);
+			SleepConditionVariableCS(&reprcondition, &repr, INFINITE);
+			LeaveCriticalSection(&repr);
 		}
+		if (tDSend == TRUE)
+			send(Server, std::string("!DbgModeOn").c_str(), std::string("!DbgModeOn").size(), 0);
+		tDSend = FALSE;
 	}
+}
+
+void RegisterValue(char Key, DWORD Value)
+{
+	SetRegister(GeneralRegister.at(Key), Value);
+}
+
+void RegisterValueFP(char Key, BOOLEAN Precision, double Value)
+{
+	SetRegisterFP(XMMRegister.at(Key), Precision, Value);
+}
+
+void CreateMap()
+{
+	GeneralRegister['0'] = GPRegisters::EAX;
+	GeneralRegister['1'] = GPRegisters::EBX;
+	GeneralRegister['2'] = GPRegisters::ECX;
+	GeneralRegister['3'] = GPRegisters::EDX;
+	GeneralRegister['4'] = GPRegisters::EDI;
+	GeneralRegister['5'] = GPRegisters::ESI;
+	GeneralRegister['6'] = GPRegisters::EBP;
+	GeneralRegister['7'] = GPRegisters::ESP;
+	GeneralRegister['8'] = GPRegisters::EIP;
+
+	XMMRegister['0'] = SSERegisters::xmm0;
+	XMMRegister['1'] = SSERegisters::xmm1;
+	XMMRegister['2'] = SSERegisters::xmm2;
+	XMMRegister['3'] = SSERegisters::xmm3;
+	XMMRegister['4'] = SSERegisters::xmm4;
+	XMMRegister['5'] = SSERegisters::xmm5;
+	XMMRegister['6'] = SSERegisters::xmm6;
+	XMMRegister['7'] = SSERegisters::xmm7;
 }
 
 DWORD WINAPI Dispatch(PVOID lpParam)
@@ -48,11 +88,9 @@ DWORD WINAPI Dispatch(PVOID lpParam)
 	connect(Server, (SOCKADDR*)&ServerAddress, sizeof(ServerAddress));
 
 	AttachRVDbg();
-
+	CreateMap();
 	AssignThread(lThreads[0]);
 	AssignThread(lThreads[1]);
-
-	SetModule(FALSE);
 
 	char buffer[128];
 	char snbuffer[128];
@@ -71,7 +109,25 @@ DWORD WINAPI Dispatch(PVOID lpParam)
 		if (receiver.substr(0, std::string("!Symbol @").length()) == std::string("!Symbol @"))
 		{
 			if (!receiver.substr(sizeof("!Symbol @ "), receiver.size()).empty())
-				Symbol = strtol(receiver.substr(sizeof("!Symbol @ "), receiver.size()).c_str(), NULL, 16);
+				sscanf(receiver.substr(sizeof("!Symbol @ "), receiver.size()).c_str(), "%X", &Symbol);
+		}
+
+		else if (receiver.substr(0, std::string("!setreg").length()) == std::string("!setreg"))
+		{
+			DWORD regv = strtol(receiver.substr(std::string("!setreg ? ").length(), receiver.length()).c_str(), NULL, 16);
+			RegisterValue(receiver.at(std::string("!setreg ").length()), regv);
+		}
+
+		else if (receiver.substr(0, std::string("!fsetreg").length()) == std::string("!fsetreg"))
+		{
+			double regv = strtod(receiver.substr(std::string("!fsetreg ? ").length(), receiver.length()).c_str(), NULL);
+			RegisterValueFP(receiver.at(std::string("!fsetreg ").length()), 0, regv);
+		}
+
+		else if (receiver.substr(0, std::string("!dsetreg").length()) == std::string("!dsetreg"))
+		{
+			double regv = strtod(receiver.substr(std::string("!dsetreg ? ").length(), receiver.length()).c_str(), NULL);
+			RegisterValueFP(receiver.at(std::string("!dsetreg ").length()), 1, regv);
 		}
 
 		else if (receiver == std::string("**ProtocolGUI"))
@@ -81,7 +137,23 @@ DWORD WINAPI Dispatch(PVOID lpParam)
 		{
 			SetExceptionMode(0);
 			size_t ExceptionElement = CheckSector(GetSector(), 128);
-			AddException(GetSector(), ExceptionElement, 0, Symbol);
+			MEMORY_BASIC_INFORMATION mbi;
+			VirtualQuery((LPVOID)Symbol, &mbi, sizeof(mbi));
+			if (mbi.Protect != PAGE_NOACCESS)
+				AddException(GetSector(), ExceptionElement, 0, Symbol);
+		}
+		else if (receiver.substr(0, std::string("!Undo ").length()) == std::string("!Undo "))
+		{
+			int index = strtol(receiver.substr(std::string("!Undo ").length(), receiver.length()).c_str(), NULL, 12);
+			if (index < GetSectorSize())
+			{
+				DWORD OldProtect;
+				VirtualProtect((LPVOID)GetSector()[index].ExceptionAddress, 1, PAGE_EXECUTE_READWRITE, &OldProtect);
+				*(DWORD*)(GetSector()[index].ExceptionAddress) = (GetSector()[index].SaveCode);
+				VirtualProtect((LPVOID)GetSector()[index].ExceptionAddress, 1, OldProtect, &OldProtect);
+				UnlockSector(GetSector(), index);
+			}
+			index = 0;
 		}
 		else if (receiver == std::string("?Breakpoint"))
 		{
@@ -89,10 +161,17 @@ DWORD WINAPI Dispatch(PVOID lpParam)
 			size_t ExceptionElement = CheckSector(GetSector(), 128);
 			AddException(GetSector(), ExceptionElement, 1, Symbol);
 		}
+
 		else if (receiver == std::string("!Get"))
 		{
-			snprintf(snbuffer, sizeof(snbuffer), "$    Symbol: 0x%02X\n", Symbol);
-			send(Server, snbuffer, sizeof(snbuffer), 0);
+			int index = SearchSector(GetSector(), GetSectorSize(), Symbol);
+			if (index < GetSectorSize())
+			{
+				snprintf(snbuffer, sizeof(snbuffer), "$    Symbol: 0x%02X\r\n    Index:%d\r\n", Symbol, index);
+				send(Server, snbuffer, sizeof(snbuffer), 0);
+			}
+			else
+				send(Server, "$    Symbol not registered", sizeof("$    Symbol not registered"), 0);
 		}
 
 		else if (receiver == std::string("!DbgGet"))
@@ -101,6 +180,10 @@ DWORD WINAPI Dispatch(PVOID lpParam)
 
 		else if (receiver == std::string("!DbgDisplayRegisters"))
 			SendDbgRegisters(Server, ProtocolGUI, GetExceptionAddress(), GetRegisters());
+		else if (receiver == std::string("!xmm-f"))
+			SendDbgRegisters(Server, 2, GetExceptionAddress(), GetRegisters());
+		else if (receiver == std::string("!xmm-d"))
+			SendDbgRegisters(Server, 3, GetExceptionAddress(), GetRegisters());
 
 		else if (receiver == std::string("!DbgRun"))
 		{
@@ -111,10 +194,8 @@ DWORD WINAPI Dispatch(PVOID lpParam)
 		else if (receiver == std::string("!Exit"))
 		{
 			if (IsAEHPresent())
-			{
 				ContinueDebugger();
-				break;
-			}
+			break;
 		}
 	}
 
@@ -127,14 +208,16 @@ DWORD WINAPI Dispatch(PVOID lpParam)
 	return 0;
 }
 
-BOOL APIENTRY DllMain( HMODULE hModule,
-                       DWORD  ul_reason_for_call,
-                       LPVOID lpReserved
-					 )
+BOOL APIENTRY DllMain(HMODULE hModule, DWORD  ul_reason_for_call, LPVOID lpReserved)
 {
 	switch (ul_reason_for_call)
 	{
 	case DLL_PROCESS_ATTACH:
+		UseModule = FALSE;
+		if (UseModule)
+			DLLInject(GetCurrentProcessId(), Filename);
+		InitializeCriticalSection(&repr);
+		InitializeConditionVariable(&reprcondition);
 		lThreads[0] = CreateThread(0, 0, Repeater, 0, 0, 0);
 		lThreads[1] = CreateThread(0, 0, Dispatch, 0, 0, 0);
 	case DLL_THREAD_ATTACH:
@@ -144,4 +227,5 @@ BOOL APIENTRY DllMain( HMODULE hModule,
 	}
 	return TRUE;
 }
+
 
