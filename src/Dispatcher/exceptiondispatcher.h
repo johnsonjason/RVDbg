ad871a66cf9b790e44bd513a417c37a0d6d7e191
Jason <jasonmjohnson1@gmail.com>
2017-10-15 15:13:16 -0400

    Update exceptiondispatcher.h

--- src/Dispatcher/exceptiondispatcher.h
+++ src/Dispatcher/exceptiondispatcher.h
@@ -4,6 +4,7 @@
 #include <tlhelp32.h>
 #include <stdlib.h>
 
+
 // Contain pointers that registers use, save them for later use aka register storage 2
 
 struct VirtualRegisters
@@ -16,8 +17,36 @@ struct VirtualRegisters
 	DWORD edi;
 	DWORD ebp;
 	DWORD esp;
+	PVOID eip;
+	BOOLEAN SSESet;
+	BOOLEAN bxmm0;
+	BOOLEAN bxmm1;
+	BOOLEAN bxmm2;
+	BOOLEAN bxmm3;
+	BOOLEAN bxmm4;
+	BOOLEAN bxmm5;
+	BOOLEAN bxmm6;
+	BOOLEAN bxmm7;
+	double dxmm0;
+	double dxmm1;
+	double dxmm2;
+	double dxmm3;
+	double dxmm4;
+	double dxmm5;
+	double dxmm6;
+	double dxmm7;
+	float xmm0;
+	float xmm1;
+	float xmm2;
+	float xmm3;
+	float xmm4;
+	float xmm5;
+	float xmm6;
+	float xmm7;
 	DWORD ReturnAddress;
 };
+
+
 
 struct PoolSect
 {
@@ -32,6 +61,7 @@ struct PoolSect
 	DWORD ExceptionOffset;
 	DWORD ReturnAddress;
 	DWORD SaveCode;
+	DWORD Index;
 };
 
 
@@ -41,14 +71,11 @@ void RaiseBreakpointException(BYTE* ptr, BYTE save, BOOLEAN on);
 void RaisePrivilegedCodeException(BYTE* ptr, BYTE save, BOOLEAN on);
 
 PVOID HandleException(PoolSect segment, const char* ModuleName);
-DWORD GetExceptionThreadId(PoolSect sector);
 size_t CheckSector(PoolSect sector[], size_t size);
 size_t SearchSector(PoolSect sector[], size_t size, DWORD address);
 void UnlockSector(PoolSect sector[], size_t index);
 void LockSector(PoolSect sector[], size_t index);
-void SuspendException(PoolSect sector);
-void ContinueException(PoolSect sector);
 void AddException(PoolSect sector[], size_t index, BOOLEAN Type, DWORD ExceptionAddress);
-size_t AccessQuery(DWORD AccessException);
+DWORD SwapAccess(DWORD AccessException, DWORD Test);
 
 #endif
