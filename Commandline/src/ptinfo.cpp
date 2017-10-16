fc98e4d1bf4fed77787e803d110e4a660547873e
Jason <jasonmjohnson1@gmail.com>
2017-10-15 23:42:22 -0400

    Create ptinfo.cpp

---
+++ Commandline/src/ptinfo.cpp
@@ -0,0 +1,27 @@
+#include "ptinfo.h"
+
+DWORD FindProcessIdFromProcessName(const std::wstring processName) {
+
+	PROCESSENTRY32 processInfo;
+	processInfo.dwSize = sizeof(processInfo);
+
+	HANDLE processesSnapshot = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, NULL);
+	if (processesSnapshot == INVALID_HANDLE_VALUE)
+		return 0;
+
+	Process32First(processesSnapshot, &processInfo);
+	if (!processName.compare(processInfo.szExeFile)) {
+		CloseHandle(processesSnapshot);
+		return processInfo.th32ProcessID;
+	}
+
+	while (Process32Next(processesSnapshot, &processInfo)) {
+		if (!processName.compare(processInfo.szExeFile)) {
+			CloseHandle(processesSnapshot);
+			return processInfo.th32ProcessID;
+		}
+	}
+
+	CloseHandle(processesSnapshot);
+	return 0;
+}
