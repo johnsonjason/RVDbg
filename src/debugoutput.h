087a746188cfb7e528d93c40ff3f78731be8f432
Jason <jasonmjohnson1@gmail.com>
2017-10-22 03:08:23 -0400

    Update debugoutput.h

--- src/debugoutput.h
+++ src/debugoutput.h
@@ -4,8 +4,12 @@
 #include <winsock.h>
 #include <stdio.h>
 #include "Dispatcher/exceptiondispatcher.h"
+#include "Dispatcher/rvdbg.h"
 
-void SendDbgRegisters(SOCKET Server, BOOLEAN Protocol, DWORD EIP, VirtualRegisters Registers);
-void SendDbgGet(SOCKET Server, BOOLEAN ExceptionType, PoolSect segment);
+namespace DbgIO
+{
+	void SendDbgRegisters(SOCKET Server, BOOLEAN Protocol, DWORD EIP, Dbg::VirtualRegisters Registers);
+	void SendDbgGet(SOCKET Server, BOOLEAN ExceptionType, Dispatcher::PoolSect segment);
+};
 
 #endif
