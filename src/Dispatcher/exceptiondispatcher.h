921fc698eb143969a976cb8b57bc82f70ba05d1f
Jason Johnson <jasonmjohnson1@gmail.com>
2018-04-21 02:17:23 -0400

    Update exceptiondispatcher.h

--- src/Dispatcher/exceptiondispatcher.h
+++ src/Dispatcher/exceptiondispatcher.h
@@ -5,30 +5,45 @@
 #include <tlhelp32.h>
 #include <stdlib.h>
 
+
 namespace Dispatcher
 {
+	/* specifies information about the exception in this section */
 	struct PoolSect
 	{
+		// copy-module name
 		char ModuleName[MAX_PATH];
+		// current thread of exception
 		HANDLE Thread;
+		// current thread id of exception
+		DWORD ThreadId;
+		// if the section is being used
 		BOOLEAN Used;
+		// if a copy-module is being used
 		BOOLEAN UseModule;
+		// is the arbitrary exception handler present for this section
 		BOOLEAN IsAEHPresent;
+		// the type of exception
 		BOOLEAN ExceptionType;
+		// the exception code information
 		DWORD ExceptionCode;
+		// the address the exception occurred at
 		DWORD ExceptionAddress;
+		// the offset of the exception address
 		DWORD ExceptionOffset;
+		// Additional information
 		DWORD ReturnAddress;
 		DWORD SaveCode;
 		DWORD Index;
 	};
+
 
 	void RaiseILGLAccessViolation(BYTE* ptr, BYTE save, BOOLEAN on);
 	void RaisePageAccessViolation(BYTE* ptr, DWORD save, BOOLEAN on);
 	void RaiseBreakpointException(BYTE* ptr, BYTE save, BOOLEAN on);
 	void RaisePrivilegedCodeException(BYTE* ptr, BYTE save, BOOLEAN on);
 
-	PVOID HandleException(PoolSect segment, const char* ModuleName);
+	PVOID Dispatcher::HandleException(Dispatcher::PoolSect segment, const char* ModuleName, BOOLEAN Constant);
 	size_t CheckSector(PoolSect sector[], size_t size);
 	size_t SearchSector(PoolSect sector[], size_t size, DWORD address);
 	void UnlockSector(PoolSect sector[], size_t index);
