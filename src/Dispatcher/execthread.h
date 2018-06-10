09613a1e5de4154986a7fd60df675100e719d66a
Jason Johnson <jasonmjohnson1@gmail.com>
2018-06-10 12:42:24 -0400

    [refactor] execthread.h

--- src/Dispatcher/execthread.h
+++ src/Dispatcher/execthread.h
@@ -2,8 +2,9 @@
 #define EXECTHREAD
 #include <windows.h>
 #include <tlhelp32.h>
+#include <cstdint>
 
-void SuspendThreads(DWORD ProcessId, DWORD ExceptThreadId);
-void ResumeThreads(DWORD ProcessId, DWORD ExceptThreadId);
+void suspend_threads(std::uint32_t process_id, std::uint32_t except_thread_id);
+void resume_threads(std::uint32_t process_id, std::uint32_t except_thread_id);
 
 #endif EXECTHREAD;
