75f9008b1d45b439e2719d0a14e1c9d8d5411de5
Jason Johnson <jasonmjohnson1@gmail.com>
2018-06-10 12:06:06 -0400

    [refactor] iatresolve.cpp
    - snake_case
    - c++ style casts
    - c++ style types (std::uint32_t)
    - some winapi redefinition using enums

--- src/IATResolver/iatresolve.cpp
+++ src/IATResolver/iatresolve.cpp
@@ -1,41 +1,41 @@
 #include "stdafx.h"
 #include "iatresolve.h"
 
-IATResolver::IMP_AT IATResolver::GetIAT(LPCSTR ModuleName)
+iat_resolution::imp_at iat_resolution::get_iat(LPCSTR module_name)
 {
-	HMODULE mod = GetModuleHandleA(ModuleName);
+	HMODULE mod = GetModuleHandleA(module_name);
 
-	PIMAGE_DOS_HEADER img_dos_headers = (PIMAGE_DOS_HEADER)mod;
-	PIMAGE_NT_HEADERS img_nt_headers = (PIMAGE_NT_HEADERS)((BYTE*)img_dos_headers + img_dos_headers->e_lfanew);
+	PIMAGE_DOS_HEADER img_dos_headers = reinterpret_cast<PIMAGE_DOS_HEADER>(mod);
+	PIMAGE_NT_HEADERS img_nt_headers = reinterpret_cast<PIMAGE_NT_HEADERS>(reinterpret_cast<std::uint8_t*>(img_dos_headers) + img_dos_headers->e_lfanew);
 
-	IATResolver::IMP_AT Retn;
-	Retn.Size = (img_nt_headers->OptionalHeader.DataDirectory[IMAGE_DIRECTORY_ENTRY_IAT].Size);
-	Retn.Address = (PVOID)((DWORD)mod + img_nt_headers->OptionalHeader.DataDirectory[IMAGE_DIRECTORY_ENTRY_IAT].VirtualAddress);
-	return Retn;
+	iat_resolution::imp_at retn;
+	retn.size = (img_nt_headers->OptionalHeader.DataDirectory[IMAGE_DIRECTORY_ENTRY_IAT].Size);
+	retn.address = reinterpret_cast<void*>((std::uint32_t)mod + img_nt_headers->OptionalHeader.DataDirectory[IMAGE_DIRECTORY_ENTRY_IAT].VirtualAddress);
+	return retn;
 }
 
-DWORD IATResolver::CalculateVirtualPageCount(IATResolver::IMP_AT IAT)
+std::uint32_t iat_resolution::calc_virtual_page_count(iat_resolution::imp_at iat)
 {
-	MEMORY_BASIC_INFORMATION MemoryBasicInformation;
-	VirtualQuery(IAT.Address, &MemoryBasicInformation, sizeof(MemoryBasicInformation));
+	MEMORY_BASIC_INFORMATION mbi;
+	VirtualQuery(iat.address, &mbi, sizeof(mbi));
 
-	DWORD Precedent = MemoryBasicInformation.RegionSize;
+	std::uint32_t precedent = mbi.RegionSize;
 
-	if (IAT.Size < Precedent)
+	if (iat.size < precedent)
 		return 1;
 
-	return (IAT.Size / Precedent);
+	return (iat.size / precedent);
 }
 
 
-void IATResolver::ResolveIAT(LPCSTR FirstModule, LPCSTR CopyModule)
+void iat_resolution::resolve_iat(LPCSTR first_module, LPCSTR copy_module)
 {
-	IATResolver::IMP_AT OriginalModuleIAT = GetIAT(FirstModule);
-	IATResolver::IMP_AT CopyModuleIAT = GetIAT(CopyModule);
-	DWORD VPC = CalculateVirtualPageCount(OriginalModuleIAT);
-	DWORD OldProtect;
+	iat_resolution::imp_at origin_module_iat = get_iat(first_module);
+	iat_resolution::imp_at copy_module_iat = get_iat(copy_module);
+	std::uint32_t vpc = calc_virtual_page_count(origin_module_iat);
+	std::uint32_t old_protect;
 
-	VirtualProtect(CopyModuleIAT.Address, VPC, PAGE_EXECUTE_READWRITE, &OldProtect);
-	memcpy(CopyModuleIAT.Address, OriginalModuleIAT.Address, OriginalModuleIAT.Size);
-	VirtualProtect(CopyModuleIAT.Address, VPC, OldProtect, &OldProtect);
+	VirtualProtect(copy_module_iat.address, vpc, static_cast<std::uint32_t>(dbg_redef::page_protection::page_xrw), reinterpret_cast<unsigned long*>(&old_protect));
+	memcpy(copy_module_iat.address, origin_module_iat.address, origin_module_iat.size);
+	VirtualProtect(copy_module_iat.address, vpc, old_protect, reinterpret_cast<unsigned long*>(&old_protect));
 }
