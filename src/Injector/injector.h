b3c3b914f88b81dc8e2a082199dac03d0e46f3a3
Jason Johnson <jasonmjohnson1@gmail.com>
2018-06-10 11:56:58 -0400

    [refactor] injector.h
    - snake_case
    - c++ style types

--- src/Injector/injector.h
+++ src/Injector/injector.h
@@ -1,7 +1,9 @@
 #ifndef INJECTOR
 #define INJECTOR
 #include <windows.h>
+#include <cstdint>
+#include "../dbgredefs.h"
 
-BOOL DLLInject(DWORD ID, const char* dll);
+bool dll_inject(std::uint32_t id, const char* dll);
 
 #endif
