e0bba977228d0e72cdf19a705778a60edd82bd6d
Jason Johnson <jasonmjohnson1@gmail.com>
2018-06-12 11:02:41 -0400

    Update exceptiondispatcher.cpp

--- src/Dispatcher/exceptiondispatcher.cpp
+++ src/Dispatcher/exceptiondispatcher.cpp
@@ -6,69 +6,32 @@
 void dispatcher::raise_instr_av(std::uint8_t* ptr, std::uint8_t save, bool on)
 {
 	std::uint32_t old_protect;
-	switch (on)
-	{
-	case false:
-		VirtualProtect(ptr, 1, static_cast<unsigned long>(dbg_redef::page_protection::page_xrw), reinterpret_cast<unsigned long*>(&old_protect));
-		*const_cast<std::uint8_t*>(ptr) = save;
-		VirtualProtect(ptr, 1, old_protect, reinterpret_cast<unsigned long*>(&old_protect));
-		return;
-	case true:
-		VirtualProtect(ptr, 1, static_cast<unsigned long>(dbg_redef::page_protection::page_xrw), reinterpret_cast<unsigned long*>(&old_protect));
-		*const_cast<std::uint8_t*>(ptr) = 0xFF;
-		VirtualProtect(ptr, 1, old_protect, reinterpret_cast<unsigned long*>(&old_protect));
-		return;
-	}
+	VirtualProtect(ptr, 1, static_cast<unsigned long>(dbg_redef::page_protection::page_xrw), reinterpret_cast<unsigned long*>(&old_protect));
+	*const_cast<std::uint8_t*>(ptr) = on ? 0xFF : save;
+	VirtualProtect(ptr, 1, old_protect, reinterpret_cast<unsigned long*>(&old_protect));
 }
 
 void dispatcher::raise_page_av(std::uint8_t* ptr, std::uint32_t save, bool on)
 {
 	std::uint32_t old_protect;
-	switch (on)
-	{
-	case false:
+	on ? VirtualProtect(ptr, 1, static_cast<unsigned long>(dbg_redef::page_protection::page_ro), reinterpret_cast<unsigned long*>(&old_protect)) :
 		VirtualProtect(ptr, 1, save, reinterpret_cast<unsigned long*>(&old_protect));
-		return;
-	case true:
-		VirtualProtect(ptr, 1, static_cast<unsigned long>(dbg_redef::page_protection::page_ro), reinterpret_cast<unsigned long*>(&old_protect));
-		return;
-	}
 }
 
 void dispatcher::raise_breakpoint_excpt(std::uint8_t* ptr, std::uint8_t save, bool on)
 {
 	std::uint32_t old_protect;
-	switch (on)
-	{
-	case false:
-		VirtualProtect(ptr, 1, static_cast<unsigned long>(dbg_redef::page_protection::page_xrw), reinterpret_cast<unsigned long*>(&old_protect));
-		*const_cast<std::uint8_t*>(ptr) = save;
-		VirtualProtect(ptr, 1, old_protect, reinterpret_cast<unsigned long*>(&old_protect));
-		return;
-	case true:
-		VirtualProtect(ptr, 1, static_cast<unsigned long>(dbg_redef::page_protection::page_xrw), reinterpret_cast<unsigned long*>(&old_protect));
-		*const_cast<std::uint8_t*>(ptr) = 0xCC;
-		VirtualProtect(ptr, 1, old_protect, reinterpret_cast<unsigned long*>(&old_protect));
-		return;
-	}
+	VirtualProtect(ptr, 1, static_cast<unsigned long>(dbg_redef::page_protection::page_xrw), reinterpret_cast<unsigned long*>(&old_protect));
+	*const_cast<std::uint8_t*>(ptr) = on ? 0xCC : save;
+	VirtualProtect(ptr, 1, old_protect, reinterpret_cast<unsigned long*>(&old_protect));
 }
 
 void dispatcher::raise_priv_code_excpt(std::uint8_t* ptr, std::uint8_t save, bool on)
 {
 	std::uint32_t old_protect;
-	switch (on)
-	{
-	case false:
-		VirtualProtect(ptr, 1, static_cast<unsigned long>(dbg_redef::page_protection::page_xrw), reinterpret_cast<unsigned long*>(&old_protect));
-		*const_cast<std::uint8_t*>(ptr) = save;
-		VirtualProtect(ptr, 1, old_protect, reinterpret_cast<unsigned long*>(&old_protect));
-		return;
-	case true:
-		VirtualProtect(ptr, 1, static_cast<unsigned long>(dbg_redef::page_protection::page_xrw), reinterpret_cast<unsigned long*>(&old_protect));
-		*const_cast<std::uint8_t*>(ptr) = 0xF4;
-		VirtualProtect(ptr, 1, old_protect, reinterpret_cast<unsigned long*>(&old_protect));
-		return;
-	}
+	VirtualProtect(ptr, 1, static_cast<unsigned long>(dbg_redef::page_protection::page_xrw), reinterpret_cast<unsigned long*>(&old_protect));
+	*const_cast<std::uint8_t*>(ptr) = on ? 0xF4 : save;
+	VirtualProtect(ptr, 1, old_protect, reinterpret_cast<unsigned long*>(&old_protect));
 }
 
 void* dispatcher::handle_exception(dispatcher::pool_sect segment, std::string module_name, bool b_module)
@@ -106,7 +69,9 @@ std::size_t dispatcher::check_sector(std::array<dispatcher::pool_sect, 128>& sec
 	for (std::size_t iterator = 0; iterator < sector.size(); iterator++)
 	{
 		if (sector[iterator].used == false)
+		{
 			return iterator;
+		}
 	}
 	return (sector.size() + 1);
 }
@@ -115,9 +80,11 @@ std::size_t dispatcher::search_sector(std::array<dispatcher::pool_sect, 128>& se
 {
 	for (std::size_t iterator = 0; iterator < sector.size(); iterator++)
 	{
-		if (sector[iterator].used == true && sector[iterator].is_aeh_present == false 
+		if (sector[iterator].used == true && sector[iterator].is_aeh_present == false
 			&& sector[iterator].dbg_exception_address == address)
+		{
 			return iterator;
+		}
 	}
 	return (sector.size() + 1);
 }
@@ -139,7 +106,7 @@ void dispatcher::lock_sector(std::array<dispatcher::pool_sect, 128>& sector, std
 void dispatcher::add_exception(std::array<dispatcher::pool_sect, 128>& sector, std::size_t index, dispatcher::exception_type type, std::uint32_t dbg_exception_address)
 {
 	sector[index].dbg_exception_address = dbg_exception_address;
-	sector[index].save_code = *(std::uint32_t*)dbg_exception_address;
+	sector[index].save_code = *reinterpret_cast<std::uint32_t*>(dbg_exception_address);
 	sector[index].dbg_exception_type = type;
 	sector[index].index = index;
 	dispatcher::lock_sector(sector, index);
@@ -147,10 +114,10 @@ void dispatcher::add_exception(std::array<dispatcher::pool_sect, 128>& sector, s
 	switch (type)
 	{
 	case dispatcher::exception_type::immediate_exception:
-		dispatcher::raise_priv_code_excpt((std::uint8_t*)sector[index].dbg_exception_address, 0, true);
+		dispatcher::raise_priv_code_excpt(reinterpret_cast<std::uint8_t*>(sector[index].dbg_exception_address), 0, true);
 		return;
 	case dispatcher::exception_type::page_exception:
-		dispatcher::raise_page_av((std::uint8_t*)sector[index].dbg_exception_address, 0, true);
+		dispatcher::raise_page_av(reinterpret_cast<std::uint8_t*>(sector[index].dbg_exception_address), 0, true);
 		return;
 	}
 
