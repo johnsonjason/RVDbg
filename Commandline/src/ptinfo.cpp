d32fcb8c626670e0e78bcf7af38b9bc24a5a9753
Jason Johnson <jasonmjohnson1@gmail.com>
2018-07-30 17:06:45 -0400

    Update ptinfo.cpp

--- Commandline/src/ptinfo.cpp
+++ Commandline/src/ptinfo.cpp
@@ -1,27 +1,33 @@
+#include "stdafx.h"
 #include "ptinfo.h"
 
-DWORD FindProcessIdFromProcessName(const std::wstring processName) {
+std::uint32_t getpid_n(const std::wstring& process_name) {
 
-	PROCESSENTRY32 processInfo;
-	processInfo.dwSize = sizeof(processInfo);
+	PROCESSENTRY32W process_info;
+	process_info.dwSize = sizeof(process_info);
 
-	HANDLE processesSnapshot = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, NULL);
-	if (processesSnapshot == INVALID_HANDLE_VALUE)
+	HANDLE processes_snapshot = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, NULL);
+
+	if (processes_snapshot == INVALID_HANDLE_VALUE)
+	{
 		return 0;
-
-	Process32First(processesSnapshot, &processInfo);
-	if (!processName.compare(processInfo.szExeFile)) {
-		CloseHandle(processesSnapshot);
-		return processInfo.th32ProcessID;
 	}
 
-	while (Process32Next(processesSnapshot, &processInfo)) {
-		if (!processName.compare(processInfo.szExeFile)) {
-			CloseHandle(processesSnapshot);
-			return processInfo.th32ProcessID;
+	Process32FirstW(processes_snapshot, &process_info);
+	if (!process_name.compare(process_info.szExeFile)) 
+	{
+		CloseHandle(processes_snapshot);
+		return process_info.th32ProcessID;
+	}
+
+	while (Process32NextW(processes_snapshot, &process_info)) 
+	{
+		if (!process_name.compare(process_info.szExeFile)) 
+		{
+			CloseHandle(processes_snapshot);
+			return process_info.th32ProcessID;
 		}
 	}
-
-	CloseHandle(processesSnapshot);
+	CloseHandle(processes_snapshot);
 	return 0;
 }
