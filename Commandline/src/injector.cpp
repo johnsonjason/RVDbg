4ec5a955f44e6b12a0a39b6a7a6d41fa18c0cc45
Jason Johnson <jasonmjohnson1@gmail.com>
2018-07-30 17:06:10 -0400

    Update injector.cpp

--- Commandline/src/injector.cpp
+++ Commandline/src/injector.cpp
@@ -1,45 +1,35 @@
+#include "stdafx.h"
 #include "injector.h"
 
-BOOL DLLInject(DWORD ID, const char* dll)
-
+std::uint32_t dll_inject(std::uint32_t id, const std::wstring& dll)
 {
-	HANDLE Process = NULL;
-	HANDLE RemoteThread = NULL;
-
-	HANDLE Objects[2] = { RemoteThread, Process };
-
-	LPVOID Memory;
-
-	LPVOID LoadLibrary;
-
-	if (!ID)
-		return false;
-
-	Process = OpenProcess(PROCESS_ALL_ACCESS, FALSE, ID);
-
-	LoadLibrary = (LPVOID)GetProcAddress(GetModuleHandleA("kernel32.dll"), "LoadLibraryA");
-
-	Memory = (LPVOID)VirtualAllocEx(Process, NULL, strlen(dll) + 1, MEM_RESERVE | MEM_COMMIT, PAGE_EXECUTE_READWRITE);
-	if (Memory == NULL)
-		return 6;
-
-	if (WriteProcessMemory(Process, (LPVOID)Memory, dll, strlen(dll) + 1, NULL) == 0)
-		return 7;
-
-	RemoteThread = CreateRemoteThread(Process, NULL, NULL, (LPTHREAD_START_ROUTINE)LoadLibrary, (LPVOID)Memory, NULL, NULL);
-	if (RemoteThread == NULL)
-		return 8;
-
-	DWORD ObjectsResult = WaitForMultipleObjects(2, Objects, TRUE, INFINITE);
-
-	if (ObjectsResult == WAIT_OBJECT_0)
+	if (!id)
 	{
-		for (auto Object : Objects)
-			CloseHandle(Objects);
-
-		VirtualFreeEx(Process, (LPVOID)Memory, 0, MEM_RELEASE);
-
-		return TRUE;
+		return false;
 	}
-	return FALSE;
+
+	HANDLE process = OpenProcess(PROCESS_ALL_ACCESS, static_cast<std::uint8_t>(false), id);
+	void* load_library = reinterpret_cast<void*>(GetProcAddress(GetModuleHandleA("kernel32.dll"), "LoadLibraryW"));
+	void* memory = const_cast<void*>(VirtualAllocEx(process, nullptr, dll.size()+1,
+		MEM_RESERVE | MEM_COMMIT, static_cast<std::uint32_t>(dbg_redef::page_protection::page_rwx)));
+
+	if (memory == nullptr)
+	{
+		return GetLastError();
+	}
+
+	if (WriteProcessMemory(process, const_cast<void*>(memory), dll.c_str(), ((dll.size()*2) + 1), nullptr) == 0)
+	{
+		return GetLastError();
+	}
+
+	HANDLE remote_thread = CreateRemoteThread(process, nullptr, dbg_redef::nullval, (LPTHREAD_START_ROUTINE)load_library,
+		const_cast<void*>(memory), dbg_redef::nullval, nullptr);
+
+	if (remote_thread == nullptr)
+	{
+		return GetLastError();
+	}
+
+	return 0;
 }
