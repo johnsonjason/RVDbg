fe83bf2cd51393bc475aa9cfc9e95ab47e3d35ea
Jason Johnson <jasonmjohnson1@gmail.com>
2018-07-30 16:30:37 -0400

    Update injector.cpp

--- src/Injector/injector.cpp
+++ src/Injector/injector.cpp
@@ -1,45 +1,35 @@
 #include "stdafx.h"
 #include "injector.h"
 
-bool dll_inject(std::uint32_t id, const char* dll)
-
+std::uint32_t dll_inject(std::uint32_t id, const std::wstring& dll)
 {
-	HANDLE process = nullptr;
-	HANDLE remote_thread = nullptr;
-	void* memory;
-	void* load_library;
-
 	if (!id)
+	{
 		return false;
+	}
 
-	process = OpenProcess(PROCESS_ALL_ACCESS, static_cast<std::uint8_t>(false), id);
-
-	load_library = reinterpret_cast<void*>(GetProcAddress(GetModuleHandleA("kernel32.dll"), "LoadLibraryA"));
-
-	memory = const_cast<void*>(VirtualAllocEx(process, nullptr, strlen(dll) + 1, 
-		MEM_RESERVE | MEM_COMMIT, static_cast<std::uint32_t>(dbg_redef::page_protection::page_xrw)));
+	HANDLE process = OpenProcess(PROCESS_ALL_ACCESS, static_cast<std::uint8_t>(false), id);
+	void* load_library = reinterpret_cast<void*>(GetProcAddress(GetModuleHandleA("kernel32.dll"), "LoadLibraryW"));
+	void* memory = const_cast<void*>(VirtualAllocEx(process, nullptr, dll.size()+1,
+		MEM_RESERVE | MEM_COMMIT, static_cast<std::uint32_t>(dbg_redef::page_protection::page_rwx)));
 
 	if (memory == nullptr)
+	{
 		return GetLastError();
+	}
 
-	if (WriteProcessMemory(process, const_cast<void*>(memory), dll, strlen(dll) + 1, nullptr) == 0)
+	if (WriteProcessMemory(process, const_cast<void*>(memory), dll.c_str(), ((dll.size()*2) + 1), nullptr) == 0)
+	{
 		return GetLastError();
+	}
 
-	remote_thread = CreateRemoteThread(process, nullptr, dbg_redef::nullval, (LPTHREAD_START_ROUTINE)load_library, 
+	HANDLE remote_thread = CreateRemoteThread(process, nullptr, dbg_redef::nullval, (LPTHREAD_START_ROUTINE)load_library,
 		const_cast<void*>(memory), dbg_redef::nullval, nullptr);
 
 	if (remote_thread == nullptr)
-		return GetLastError();
-
-	std::uint32_t objects_result = WaitForSingleObject(remote_thread, dbg_redef::infinite);
-
-	if (objects_result == WAIT_OBJECT_0)
 	{
-		CloseHandle(process);
-		CloseHandle(remote_thread);
-		VirtualFreeEx(process, const_cast<void*>(memory), 0, MEM_RELEASE);
-		return true;
+		return GetLastError();
 	}
 
-	return false;
+	return 0;
 }
