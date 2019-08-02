52dc7a59103aac2e2470a6f67073fcc837bbb6eb
Jason Johnson <jasonmjohnson1@gmail.com>
2019-08-02 02:28:39 -0400

    Create exception_store.h

---
+++ src/exception_store.h
@@ -0,0 +1,46 @@
+#ifndef EXCEPTION_STORE_H
+#define EXCEPTION_STORE_H
+
+#include <Windows.h>
+#include <vector>
+
+namespace Debugger
+{
+	//
+	// Enum specifying the state of the exception condition
+	// ImmediateMode - Execution interruption based on injection
+	// ModuleMode - Execution interruption based on page exceptions
+	//
+
+	typedef enum _EXCEPTION_STATE : BYTE
+	{
+		ImmediateMode,
+		PageMode,
+		ModuleMode
+	} EXCEPTION_STATE;
+
+	//
+	// Stores the registered exception
+	// SaveCode is the operation code prior to an immediate exception injection
+	// ConditionAddress is the address for the exception
+	// Mode is the type of exception condition
+	// ModulePtr is the cloned module to use if specified
+	//
+
+	typedef struct _EXCEPTION_BASE
+	{
+		EXCEPTION_STATE Mode;
+		BYTE SaveCode;
+		BOOLEAN UseSave;
+		DWORD_PTR ConditionAddress;
+		DWORD_PTR ModulePtr;
+	} EXCEPTION_BASE;
+
+	extern std::vector<EXCEPTION_BASE> ExceptionStore;
+
+	void RegisterExceptionCondition(DWORD_PTR ExceptionAddress, EXCEPTION_STATE Mode);
+	bool CloseExceptionCondition(DWORD_PTR ExceptionAddress, EXCEPTION_STATE Mode);
+	bool DiscoverExceptionCondition(DWORD_PTR ExceptionAddress, EXCEPTION_STATE Mode);
+}
+
+#endif
