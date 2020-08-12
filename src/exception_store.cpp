c9c9f8ed11c5c7d022dcfaa9ea23b556f93be152
Jason Johnson <jasonmjohnson1@gmail.com>
2020-08-11 23:40:09 -0400

    Update exception_store.cpp

--- src/exception_store.cpp
+++ src/exception_store.cpp
@@ -12,9 +12,12 @@ std::vector<Debugger::EXCEPTION_BASE> Debugger::ExceptionStore;
  Routine Description:
 
 	Uses the BaseException parameter and changes the protection on .ConditionAddress so it can be written
-	Modifies the address in the BaseException to cause an exception based on a privileged instruction (HLT) 
+	Modifies the address in the BaseException to cause an exception based on a privileged instruction (HLT) : UseSave = FALSE
 	OR
-	Modifies the address in the BaseException to the operation code prior to an exception
+	Modifies the address in the BaseException to the operation code (SaveCode) prior to an exception : UseSave = TRUE
+
+	This is a routine that occurs a two points of time, one which is prior to an exception being handled, and one point in time that is post-handling.
+	This is why the SaveCode and UseSave optins exist within EXCEPTION_BASE
 
 --*/
 
@@ -22,8 +25,41 @@ static void SetHltCondition(Debugger::EXCEPTION_BASE& BaseException)
 {
 	DWORD OldProtection;
 	VirtualProtect(reinterpret_cast<PVOID>(BaseException.ConditionAddress), 1, PAGE_EXECUTE_READWRITE, &OldProtection);
-	*reinterpret_cast<PBYTE>(BaseException.ConditionAddress) = BaseException.UseSave ? BaseException.SaveCode : 0xFF;
+
+	//
+	// If UseSave is false, then write the instruction to trigger an exception (hlt)
+	// If UseSave is true, then write the instruction that should actually be there (correct instruction / integrity)
+	//
+
+	*reinterpret_cast<PBYTE>(BaseException.ConditionAddress) = BaseException.UseSave ? BaseException.SaveCode : 0xF4;
+
 	VirtualProtect(reinterpret_cast<PVOID>(BaseException.ConditionAddress), 1, OldProtection, &OldProtection);
+}
+
+/*
+
+Routine Description:
+
+	Handles the exception from the debugger and restores information about the exception point prior
+
+Parameters:
+
+	BaseException - The structure that contains information about the exception utilized by the debugger
+
+*/
+void Debugger::HandleException(Debugger::EXCEPTION_BASE& BaseException)
+{
+	switch (BaseException.Mode)
+	{
+	case Debugger::EXCEPTION_STATE::ImmediateMode:
+		BaseException.UseSave = true;
+		SetHltCondition(BaseException);
+		break;
+	case Debugger::EXCEPTION_STATE::ModuleMode:
+		break;
+	case Debugger::EXCEPTION_STATE::PageMode:
+		break;
+	}
 }
 
 /*++
@@ -41,11 +77,30 @@ Parameters:
 
 void Debugger::RegisterExceptionCondition(DWORD_PTR ExceptionAddress, Debugger::EXCEPTION_STATE Mode)
 {
+	//
+	// Store exception conditions within the record and push it into the exception store
+	// The exception store will be linearly searched by the debugger for the exception
+	//
+
 	Debugger::EXCEPTION_BASE Record;
+	MEMORY_BASIC_INFORMATION AddressQuery = { 0 };
+
+	DWORD QueryResult = VirtualQuery(reinterpret_cast<PDWORD_PTR>(ExceptionAddress), &AddressQuery, sizeof(AddressQuery));
+	if (!QueryResult || AddressQuery.Protect == PAGE_NOACCESS)
+	{
+		return;
+	}
+
 	Record.UseSave = FALSE;
 	Record.ConditionAddress = ExceptionAddress;
 	Record.SaveCode = *reinterpret_cast<PBYTE>(ExceptionAddress);
 	Record.Mode = Mode;
+
+	if (ExceptionStore.size() > INT_MAX)
+	{
+		return;
+	}
+
 	ExceptionStore.push_back(Record);
 
 	switch (Mode)
@@ -107,15 +162,14 @@ Return Value:
 
 --*/
 
-bool Debugger::DiscoverExceptionCondition(DWORD_PTR ExceptionAddress, Debugger::EXCEPTION_STATE Mode)
+int Debugger::DiscoverExceptionCondition(DWORD_PTR ExceptionAddress, Debugger::EXCEPTION_STATE Mode)
 {
 	for (std::vector<Debugger::EXCEPTION_BASE>::iterator iRecord = ExceptionStore.begin(); iRecord != ExceptionStore.end(); ++iRecord)
 	{
 		if (iRecord->ConditionAddress == ExceptionAddress && iRecord->Mode == Mode)
 		{
-			return true;
+			return std::distance(ExceptionStore.begin(), iRecord);
 		}
 	}
-	return false;
+	return 0;
 }
-
