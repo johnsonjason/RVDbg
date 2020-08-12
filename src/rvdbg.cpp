c66b3318e51aec795d72c0d8f20b5bca6545927b
Jason Johnson <jasonmjohnson1@gmail.com>
2020-08-11 23:37:51 -0400

    Update rvdbg.cpp

--- src/rvdbg.cpp
+++ src/rvdbg.cpp
@@ -10,6 +10,9 @@ Debugger::PROCESS_STATE ProcessState;
 Debugger::DebuggerSnapshot* ActiveSnapshot = NULL;
 PVOID RealKiUserExceptionDispatcher;
 PVOID RetDebuggerAddress;
+DWORD DebugInputThreadId = NULL;
+bool Debugger::DebuggerPauseCondition = false;
+static dio::Client* DebugInputRepeater = NULL;
 
 /*++
 
@@ -31,7 +34,7 @@ static PVOID ScanKiSignature(PVOID Base)
 {
 	for (size_t iBase = reinterpret_cast<size_t>(Base); iBase < 128; iBase++)
 	{
-		if (*reinterpret_cast<PDWORD>(iBase) == KI_DISPATCH_SIGNATURE)
+		if (*reinterpret_cast<DWORD*>(iBase) == KI_DISPATCH_SIGNATURE)
 		{
 			return reinterpret_cast<PVOID>(iBase);
 		}
@@ -46,12 +49,24 @@ Routine Description:
 	Initializes information for the debugger
 	Imports routines for use at run-time
 
+Parameters:
+	
+	InputThreadId - The thread id of the thread that takes input for the debugger and translates it to certain functions (I.e triggering a breakpoint.)
+
+	Repeater - The debugger client ptr, to be used by the debugger to notify the client that the debugger is active or inactive
+
+Return Value:
+
+	None
+
 --*/
 
-void Debugger::InitializeDebugInfo()
+void Debugger::InitializeDebugInfo(DWORD InputThreadId, dio::Client* Repeater)
 {
-	RealKiUserExceptionDispatcher = reinterpret_cast<PVOID>(GetProcAddress(GetModuleHandleW(L"ntdll.dll"), "KiUserExceptionDispatcher"));
-	HookAPIRoutine(ScanKiSignature(RealKiUserExceptionDispatcher), SaveRegisterState, "ntdll:KiUserExceptionDispatcher");
+	DebugInputThreadId = InputThreadId;
+	RealKiUserExceptionDispatcher = reinterpret_cast<PVOID>((DWORD_PTR)GetProcAddress(GetModuleHandleW(L"ntdll.dll"), "KiUserExceptionDispatcher") + 0x18);
+	HookAPIRoutine(RealKiUserExceptionDispatcher, SaveRegisterState, "ntdll:KiUserExceptionDispatcher");
+	DebugInputRepeater = Repeater;
 }
 
 /*++
@@ -92,90 +107,6 @@ void Debugger::SetProcessState(PROCESS_STATE State)
 	ProcessState = State;
 }
 
-/*++
-
-Routine Description:
-
-	Suspend all threads in the process during debug mode, except the debug control thread 
-	Later resume the networking and general control threads
-
-Parameters:
-
-	ExceptionThreadId - The thread id of the debug control thread to be exempt
-
---*/
-
-static void SuspendThreads(DWORD_PTR ExceptionThreadId)
-{
-	HANDLE Snapshot = CreateToolhelp32Snapshot(TH32CS_SNAPTHREAD, 0);
-	if (Snapshot != INVALID_HANDLE_VALUE)
-	{
-		THREADENTRY32 ThreadEntry;
-		ThreadEntry.dwSize = sizeof(ThreadEntry);
-		if (Thread32First(Snapshot, &ThreadEntry))
-		{
-			do
-			{
-				if (ThreadEntry.dwSize >= FIELD_OFFSET(THREADENTRY32, th32OwnerProcessID) + sizeof(ThreadEntry.th32OwnerProcessID))
-				{
-					if (ThreadEntry.th32ThreadID != ExceptionThreadId && ThreadEntry.th32OwnerProcessID == GetCurrentProcessId())
-					{
-						HANDLE Thread = OpenThread(THREAD_ALL_ACCESS, FALSE, ThreadEntry.th32ThreadID);
-						if (Thread != NULL)
-						{
-							SuspendThread(Thread);
-							CloseHandle(Thread);
-						}
-					}
-				}
-				ThreadEntry.dwSize = sizeof(ThreadEntry);
-			} while (Thread32Next(Snapshot, &ThreadEntry));
-		}
-		CloseHandle(Snapshot);
-	}
-}
-
-/*++
-
-Routine Description:
-
-	Resume all threads in the process during debug mode
-
-Parameters:
-
-	ExceptionThreadId - The thread id that was exempt during suspension
-
---*/
-
-static void ResumeThreads(DWORD ExceptionThreadId)
-{
-	HANDLE Snapshot = CreateToolhelp32Snapshot(TH32CS_SNAPTHREAD, 0);
-	if (Snapshot != INVALID_HANDLE_VALUE)
-	{
-		THREADENTRY32 ThreadEntry;
-		ThreadEntry.dwSize = sizeof(ThreadEntry);
-		if (Thread32First(Snapshot, &ThreadEntry))
-		{
-			do
-			{
-				if (ThreadEntry.dwSize >= FIELD_OFFSET(THREADENTRY32, th32OwnerProcessID) + sizeof(ThreadEntry.th32OwnerProcessID))
-				{
-					if (ThreadEntry.th32ThreadID != ExceptionThreadId && ThreadEntry.th32OwnerProcessID == GetCurrentProcessId())
-					{
-						HANDLE Thread = OpenThread(THREAD_ALL_ACCESS, FALSE, ThreadEntry.th32ThreadID);
-						if (Thread != NULL)
-						{
-							ResumeThread(Thread);
-							CloseHandle(Thread);
-						}
-					}
-				}
-				ThreadEntry.dwSize = sizeof(ThreadEntry);
-			} while (Thread32Next(Snapshot, &ThreadEntry));
-		}
-		CloseHandle(Snapshot);
-	}
-}
 
 /*++
 
@@ -232,32 +163,93 @@ Routine Description:
 	Module Mode Dispatcher - Creates a cloned module which will be used in page exceptions. Page exceptions
 	are used with NO_ACCESS and redirect static code to a cloned module
 
-*/
+Return Value:
+
+	The address of the exception, if it returns zero then statesave.asm 
+	will pass the exception to the regular Windows exception handler execution path
+
+--*/
 
 PVOID EnterDebugState()
 {
-	if (!Debugger::DiscoverExceptionCondition(GlobalContext.dwExceptionComparator, Debugger::EXCEPTION_STATE::ImmediateMode))
+	int ExceptionIndex = Debugger::DiscoverExceptionCondition(GlobalContext.dwExceptionComparator, Debugger::EXCEPTION_STATE::ImmediateMode);
+	if (ExceptionIndex < 0)
 	{
 		if (!Debugger::DiscoverExceptionCondition(GlobalContext.dwExceptionComparator, Debugger::EXCEPTION_STATE::ModuleMode))
 		{
+			//
+			// NOT IMPLEMENTED
+			//
+
 			return NULL;
 		}
-		return ModuleModeDispatcher();
+		else
+		{
+			return ModuleModeDispatcher();
+		}
 	}
+
+	//
+	// Restore the original instruction at the address of exception, restore data (e.g. page properties)
+	//
+
+	Debugger::HandleException(Debugger::ExceptionStore.at(ExceptionIndex));
+
+	//
+	// Suspend all threads, except the one thread passed as an Id 
+	// Create a snapshot of the debug context and set it as the active state
+	//
 
 	SuspendThreads(GetCurrentThreadId());
 	Debugger::DebuggerSnapshot* Snapshot = new Debugger::DebuggerSnapshot(GlobalContext, SSEGlobalContext);
 	ActiveSnapshot = Snapshot;
 
+	//
+	// Check if continuous mode is enabled, this is useful for subtle hooking
+	//
+
 	if (Debugger::GetProcessState() == Debugger::PROCESS_STATE::Continuous)
 	{
-		
+		ResumeThreads(GetCurrentThreadId());
 		return reinterpret_cast<PVOID>(GlobalContext.dwEip);
 	}
 
+	//
+	// Handle global context values that are not obtained from statesave.asm
+	//
+
+	GlobalContext.dwEip = Debugger::ExceptionStore.at(ExceptionIndex).ConditionAddress;
+
+	//
+	// Notify the debugger client that it can take input
+	// Resume the debugger thread that takes input
+	//
+
+	DebugInputRepeater->SendString("DebuggerOn");
+	HANDLE DebugThread = OpenThread(THREAD_SUSPEND_RESUME, FALSE, DebugInputThreadId);
+	ResumeThread(DebugThread);
+	CloseHandle(DebugThread);
+
+	//
+	// Set debugger event condition values and wait for a change notification
+	//
+
+	Debugger::DebuggerPauseCondition = true;
+	bool SaveWait = Debugger::DebuggerPauseCondition;
+	WaitOnAddress(&Debugger::DebuggerPauseCondition, &SaveWait, sizeof(Debugger::DebuggerPauseCondition), INFINITE);
+
+	//
+	// Struct assignment from the snapshot to the global processor context (global- relative to the debugged process)
+	// We won't be using the snapshot in immediate mode further then when it is in demand, so we will delete it
+	// Notify the debugger client that debugging has stopped and resume all threads.
+	//
+
+	ActiveSnapshot->CopyToContext(GlobalContext, SSEGlobalContext);
 	ActiveSnapshot = NULL;
 	delete Snapshot;
 
+	DebugInputRepeater->SendString("DebuggerOff");
+	ResumeThreads(GetCurrentThreadId());
 	Debugger::CloseExceptionCondition(GlobalContext.dwExceptionComparator, Debugger::EXCEPTION_STATE::ImmediateMode);
 	return reinterpret_cast<PVOID>(GlobalContext.dwEip);
 }
